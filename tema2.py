d43916d22b95b3a68fa63e7b1f0b5d9c26b4be7e
solanav <solanav@qq.com>
2020-07-04 18:46:10 +0200

    Riesgos RAW, WAR y WAW funcionando debuti

--- tema2.py
+++ tema2.py
@@ -3,14 +3,13 @@ from dataclasses import dataclass
 branch = [
     "beq",
     "bnq",
-    "jump",
-    "jp",
 ]
 
 lw = [
     "lw",
     "load",
     "st",
+    "ld"
 ]
 
 sq = [
@@ -26,11 +25,14 @@ rtype = [
     "nor",
 ]
 
+
 @dataclass
 class Instruction:
+    id: int
     name: str
     writes_to: []
     reads_from: []
+
 
 def parse_ins(fname):
     instructions = open(fname).read().split("\n")
@@ -43,6 +45,8 @@ def parse_ins(fname):
             nus.append(part.lower())
         pins.append(nus)
 
+    nu_pins = []
+    id = 1
     for i in pins:
         writes_to = []
         reads_from = []
@@ -56,40 +60,60 @@ def parse_ins(fname):
         # Si la instruccion es de tipo load
         elif i[0] in lw:
             writes_to.append(i[1])
-            cleani = i.replace(")", "").split("(")[-1]
+            cleani = i[2].replace(")", "").split("(")[-1]
             reads_from.append(cleani)
 
         # Si la instruccion es de tipo store
         elif i[0] in lw:
             reads_from.append(i[1])
-            cleani = i.replace(")", "").split("(")[-1]
+            cleani = i[2].replace(")", "").split("(")[-1]
             writes_to.append(cleani)
 
-        # Si la instruccion es de tipo salto
-        # No escribimos ni mierdas
+        # Si la instruccion es de tipo branch
+        elif i[0] in branch:
+            reads_from.append(i[1])
+            reads_from.append(i[2])
 
-        pins.append(Instruction(i[0], writes_to, reads_from))
+        nu_pins.append(Instruction(id, i[0], writes_to, reads_from))
+        id += 1
 
-
-    return pins
+    return nu_pins
 
 
 def find_hazards(fname, von_neuman=True):
     instructions = parse_ins(fname)
 
-    raw = []
-    war = []
-    waw = []
-
     for curr_ins in instructions:
-        print("Checking {}".format(curr_ins[0]))
-
         for re_ins in instructions:
             if re_ins == curr_ins:
                 break
 
-            print("\tChecking with {}".format(re_ins[0]))
+            # Check RAW
+            for w in curr_ins.reads_from:
+                if w in re_ins.writes_to:
+                    print("RAW hazard, I{} con I{} por {}".format(
+                        curr_ins.id,
+                        re_ins.id,
+                        w.upper()
+                    ))
 
+            # Check WAR
+            for w in curr_ins.writes_to:
+                if w in re_ins.reads_from:
+                    print("WAR hazard, I{} con I{} por {}".format(
+                        curr_ins.id,
+                        re_ins.id,
+                        w.upper()
+                    ))
+
+            # Check WAW
+            for w in curr_ins.writes_to:
+                if w in re_ins.writes_to:
+                    print("WAW hazard, I{} con I{} por {}".format(
+                        curr_ins.id,
+                        re_ins.id,
+                        w.upper()
+                    ))
 
 def main():
     find_hazards("data.asm", von_neuman=True)
